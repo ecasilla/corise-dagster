60ece203c572e1da602ebadf626b5fd632ffa14c
Ernie Casilla <ernie@vaultconsultingroup.com>
2023-04-16 14:22:33 +0000

    feat: week 2

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -14,31 +14,66 @@ from dagster import (
 from workspaces.config import REDIS, S3, S3_FILE
 from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
+from operator import attrgetter
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={"s3_key": String},
+    required_resource_keys={"s3"},
+    tags={"kind": "s3"},
+    out={"stocks": Out(dagster_type=List[Stock])},
+)
+def get_s3_data(context: OpExecutionContext) -> List[Stock]:
+    s3 = context.resources.s3
+    stock_strings = s3.get_data(context.op_config["s3_key"])
+    return list(map(Stock.from_list, stock_strings))
 
 
-@op
-def process_data():
-    pass
+@op(
+    ins={"stocks": In(dagster_type=List[Stock])},
+    out={"aggregation": Out(dagster_type=Aggregation)},
+    description="Return the Aggregation with the highest stock price",
+)
+def process_data(context, stocks) -> Aggregation:
+    highest_stock = max(stocks, key=attrgetter("high"))
+    return Aggregation(date=highest_stock.date, high=highest_stock.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(
+    required_resource_keys={"redis"},
+    tags={"kind": "redis"},
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    out=Out(Nothing),
+    description="Upload an Aggregation to Redis",
+)
+def put_redis_data(context, aggregation):
+    redis = context.resources.redis
+    redis.put_data(  
+        name=String(aggregation.date),
+        value=String(aggregation.high)
+    )
 
 
-@op
-def put_s3_data():
-    pass
+@op(
+    required_resource_keys={"s3"},
+    tags={"kind": "s3"}, 
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    out=Out(Nothing),
+)
+def put_s3_data(context, aggregation):
+    s3 = context.resources.s3
+    s3.put_data(
+        name=String(aggregation.date),
+        data=aggregation
+    )
 
 
 @graph
 def machine_learning_graph():
-    pass
+    stocks = get_s3_data()
+    aggregation = process_data(stocks)
+    put_redis_data(aggregation)
+    put_s3_data(aggregation)
 
 
 local = {
